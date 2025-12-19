7ce8d76b4e78ffdf7cdb64236d8437e689bf89de
tivvvv <18612553935@163.com>
2025-12-19 23:32:02 +0800

    feat: 基于Netty实现WebSocket通信.

---
+++ im-study-realtime-communicate/src/main/java/com/tiv/im/study/realtime/communicate/RealtimeCommunicateApplication.java
@@ -0,0 +1,13 @@
+package com.tiv.im.study.realtime.communicate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeCommunicateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimeCommunicateApplication.class, args);
+    }
+
+}
---
+++ im-study-realtime-communicate/src/main/java/com/tiv/im/study/realtime/communicate/websocket/MessagedHandler.java
@@ -0,0 +1,18 @@
+package com.tiv.im.study.realtime.communicate.websocket;
+
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Sharable
+public class MessagedHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame textWebSocketFrame) throws Exception {
+        log.info("接收到消息: {}", textWebSocketFrame.text());
+    }
+
+}
